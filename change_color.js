514b8cc3c3147c1e0bb5be12ffbfb9dc007d8f17
Bruce-Yu2017 <ilywanzi@gmail.com>
2018-05-23 19:30:02 -0700

    new

--- change_color.js
+++ change_color.js
@@ -1,17 +1,59 @@
-let randomColor = (int) => {
+var randomColor = (int) => {
   return Math.floor(Math.random() * 251);
 }
 
-let randomCell = (num) => {
+var randomCell = (num) => {
   return Math.floor(Math.random() * 17)
 }
 
-setInterval(() => {
-  let cell = document.getElementById(randomCell(17).toString());
-  // console.log(cell);
-  let r = randomColor(255);
-  let g = randomColor(255);
-  let b = randomColor(255);
-  cell.style.backgroundColor = `rgb(${r},${g},${b})`;
-}, 250)
+var queue = [];
+var arr = [];
+for(var i = 1; i <= 16; i++) {
+  arr.push(i);
+}
+console.log("arr: ", arr)
+
+var changeColor = () => {
+  var r = randomColor(255);
+  var g = randomColor(255);
+  var b = randomColor(255);
+  if(queue.length < 9) {
+    var randomIdx = Math.floor(Math.random() * arr.length);
+    // console.log('randomIdx: ', randomIdx);
+
+    var selectedCellNum = arr[randomIdx];
+    // console.log('selectedCellNum: ', selectedCellNum);
+
+    var selectedCell = document.getElementById(selectedCellNum.toString());
+    // console.log('selectedCell: ', selectedCell);
+
+    selectedCell.style.backgroundColor = `rgb(${r},${g},${b})`;
+    arr.splice(randomIdx, 1);
+    // console.log('arr: ', arr);
+    queue.push(selectedCellNum);
+    // console.log('queue: ', queue);
+  }
+  else {
+    console.log("???")
+    var first = queue.shift();
+    arr.push(first);
+    var randomIdx = Math.floor(Math.random() * arr.length);
+    // console.log('randomIdx: ', randomIdx);
+
+    var selectedCellNum = arr[randomIdx];
+    // console.log('selectedCellNum: ', selectedCellNum);
+
+    var selectedCell = document.getElementById(selectedCellNum.toString());
+    // console.log('selectedCell: ', selectedCell);
+
+    selectedCell.style.backgroundColor = `rgb(${r},${g},${b})`;
+    arr.splice(randomIdx, 1);
+    // console.log('arr: ', arr);
+    queue.push(selectedCellNum);
+    // console.log('queue: ', queue);
+  }
+}
+
+
+setInterval(changeColor, 1000)
 
